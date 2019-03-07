f1ef89ff5f372bcf25d9fe34aa59436b65139716
amlakgirma <amlakgirma@yahoo.com>
2019-03-07 18:33:54 -0500

    adding logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,3 @@
-//array with all cards
 var cards = [
  {
    rank: "queen",
@@ -22,10 +21,10 @@ var cards = [
  }
 ];
 
-//array with cards that are in play
+
 var cardsInPlay = [];
 
-//function to store the steps to check for a match.
+
 var checkForMatch = function () {
  if (cardsInPlay.length === 2) {
    if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -37,35 +36,32 @@ var checkForMatch = function () {
 };
 
 
-//function to store all steps that should happen when the user flips a card.
 var flipCard = function () {
  var cardId = this.getAttribute('data-id');
- this.setAttribute( src, cards[cardId].cardImage);
+ this.setAttribute(src, cards[cardId].cardImage);
 
- //push the cards that the user flipped to the cardsInPlay array
- cardsInPlay.push(cards[cardId].rank)
+  cardsInPlay.push(cards[cardId].rank);
+ cardsInPlay.push(cards[cardId].suit);
+ cardsInPlay.push(cards[cardId].cardImage);
 
- //test to see if the code is run
  console.log("User flipped " + cards[cardId].rank)
  console.log(cards[cardId].cardImage)
  console.log(cards[cardId].suit)
 
- //call the function to check for a match
+
  checkForMatch ();
 };
 
-//function new game board
+
 var createBoard = function () {
 for (var i = 0; i < cards.length; i++){
  var cardElement = document.createElement('img');
  cardElement.setAttribute('src', 'images/back.png');
  cardElement.setAttribute('data-id', i);
  cardElement.addEventListener('click', flipCard);
-  document.getElementById('game-board').appendChild(cardElement);
-
-
+ window.document.getElementById('game-board').appendChild(cardElement);
 }
 };
 
-//call the function game board
+
 createBoard();
