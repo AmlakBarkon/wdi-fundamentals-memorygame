2a47672663c48ccdf975d3bb1d002e1daf010a01
amlakgirma <amlakgirma@yahoo.com>
2019-03-07 13:18:25 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,16 +7,22 @@
 // console.log("User flipped " + carTwo);
 var cards = ["queen", "king", "queen", "king"];
 var cardInPlay = [];
-var cardOne = cardInPlay.push(cards[0]);
-var cardTwo = cardInPlay.push(cards[1]);
-var cardThree = cardInPlay.push(cards[2]);
-var cardFour = cardInPlay.push(cards[3]);
-console.log("User flipped " + cardInPlay[cardOne]);
-console.log("User flipped " + cardInPlay[cardTwo]);
-if( cardInPlay.length === 2) {
- if(cardInPlay[0] === cardInPlay[2]){
-   alert("You found a match");
+function checkForMatch() {
+  if(cardInPlay[0] === cardInPlay[2]){
+    alert("You found a match");
+  }
+  else {
+   alert("Sorry, try again");
  }
-} else {
-  alert("Sorry, try again");
+
 }
+function flipCard( cardId){
+  console.log("The user flipped " +cards[cardId]);
+
+  if( cardInPlay.length === 2) {
+       checkForMatch();
+
+}
+}
+flipCard(1);
+flipCard(3);
