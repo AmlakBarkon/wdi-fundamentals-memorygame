e990d67e3a6db9a8a9eb4ec554a3392634bc01b6
amlakgirma <amlakgirma@yahoo.com>
2019-03-07 12:55:27 -0500

    adding if condition

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,120 +1,22 @@
 
-// var cards = [
-  // {
-  //   rank: "queen",
-  //   suit: 'hearts',
-  //   cardImage: "images/queen-of-hearts.png"
-  // },
-  // {
-  //   rank: "queen",
-  //   suit: "diamonds",
-  //   cardImage: "images/queen-of-diamonds.png"
-  // },
-  // {
-  //   rank: "king",
-  //   suit: "hearts",
-  //   cardImage: "images/king-of-hearts.png"
-  // },
-  // {
-  //   rank: "king",
-  //   suit: "diamonds",
-  //   cardImage: "images/king-of-diamonds.png"
-  // }
-// ];
-// var cardsInPlay = [];
-// function checkForMatch() {
-//
-//  setAttribute("src", cards[cardId].cardImage);
-//   if(cardsInPlay[0]=== cardsInPlay[1]){
-//     console.log(cards[cardId].cardImage);
-//
-//     console.log("You found the match");
-//   } else {
-//     alert("Sorry, try again");
-//   }
-// }
-// var flipCard = function() {
-//   var cardId = this.getAttribute("data-id");
-//    cardsInPlay.push(cards[cardId].rank);
-//    cardsInPlay.push(cards[cardId].suit);
-//    cardsInPlay.push(cards[cardId].cardImage);
-//     console.log("User Flipped " + cards[cardId].rank);
-//     console.log("User Flipped " + cards[cardId].suit);
-//     console.log("User Flipped " + cards[cardId].cardImage);
-//     if(crdsInPlay.length === 2){
-//       checkForMatch();
-//     }
-// }
-// function createBoard() {
-//   for(var i = 0; i < 3; i ++) {
-//     var cardElement = document.createElement('img');
-//         cardElement.setAttribute("src", "images/back.png");
-//         cardElement.setAttribute("data-id", i);
-//         cardElement.addEventListener("Click", flipCard);
-//         //sdocument.getElementById("game-board").appendChild(cardElement);
-//         flipCard();
-//
-//   }
-// }
-//
-// createBoard();
-//**********************************
-
-var cards = [
-  {
-  rank: "queen",
-  suit: 'hearts',
-  cardImage: "images/queen-of-hearts.png"
-},
-{
-  rank: "queen",
-  suit: "diamonds",
-  cardImage: "images/queen-of-diamonds.png"
-},
-{
-  rank: "king",
-  suit: "hearts",
-  cardImage: "images/king-of-hearts.png"
-},
-{
-  rank: "king",
-  suit: "diamonds",
-  cardImage: "images/king-of-diamonds.png"
-}
-
-];
-var cardsInPlay = [];
-var checkForMatch = function() {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
+// var carOne = "queen";
+// var carTwo = "queen";
+// var carThree = "king";
+// var carFour = "king";
+// console.log("User flipped " + carFour);
+// console.log("User flipped " + carTwo);
+var cards = ["queen", "king", "queen", "king"];
+var cardInPlay = [];
+var cardOne = cardInPlay.push(cards[0]);
+var cardTwo = cardInPlay.push(cards[1]);
+var cardThree = cardInPlay.push(cards[2]);
+var cardFour = cardInPlay.push(cards[3]);
+console.log("User flipped " + cardInPlay[cardOne]);
+console.log("User flipped " + cardInPlay[cardTwo]);
+if( cardInPlay.length === 2) {
+ if(cardInPlay[0] === cardInPlay[2]){
+   alert("You found a match");
+ }
 } else {
-alert("Sorry, try again.");
+  alert("Sorry, try again");
 }
-}
-var flipCard = function() {
-      var cardId = this.getAttribute("data-id");
-     console.log("User Flipped " + cards[cardId].rank + "\n" + cards[cardId].suit + "\n" +cards[cardId].cardImage);
-       cardsInPlay.push(cards[cardId].rank);
-       cardsInPlay.push(cards[cardId].suit);
-       cardsInPlay.push(cards[cardId].cardImage);
-       cardsInPlay.setAttribute("src", "cards[cardId].cardImage")
-     if(cardsInPlay.length === 2){
-
-         checkForMatch();
-       } else {
-
-       }
-
-}
-
-function createBoard() {
-  for( var i = 0; i < cards.length; i++){
-    var cardElement = document.createElement("img");
-        cardElement.setAttribute('src', 'images/back.png');
-       cardElement.setAttribute("data-id",i);
-       cardElement.addEventListener('click', flipCard);
-      document.getElementById("game-board").appendChild(cardElement);
-   console.log(cardElement);
-  }
-}
-createBoard();
